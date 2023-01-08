634d35af11121d77ca0d964d806d9b8aa85e332f
nithin-hunt <nithinpsgice@gmail.com>
2023-01-08 20:06:55 +0530

    ngrok connection

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+//connecting the ngrok 
+server.listen(port, () => console.log(`Server running on port ${port}`));
+(async function(){
+    const url = await ngrok.connect({
+        proto:'http',
+        addr:port,
+        authtoken:'2K31YlIKGmdMuvjkoFnKxscuFbj_2ye29yULpfzHSogpDE1np'
+    });
+    console.log(url);
+})();
